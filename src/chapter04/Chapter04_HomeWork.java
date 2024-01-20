2f65b72bffecc5c1f4254ae92ec07260ed0afd22
LuckyE <1024053774@qq.com>
2024-01-20 22:33:37 +0800

    Chapter04_HomeWork6

--- src/chapter04/Chapter04_HomeWork.java
+++ src/chapter04/Chapter04_HomeWork.java
@@ -89,3 +89,21 @@ class Chapter04_HomeWork4{
 
     }
 }
+
+class Chapter04_HomeWork6{
+    //输出1-100之间的不能被5整除的数，每5个一行
+    public static void main(String[] args) {
+        int count = 0;
+    for( int i = 1;i <= 100;i++){
+            if (i % 5 != 0){
+                System.out.print(i);
+                System.out.print('\t');
+                count++;
+                while (count == 5){
+                    System.out.println();
+                    count = 0;
+                }
+            }
+    }
+    }
+}
