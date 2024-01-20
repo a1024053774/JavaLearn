0b681b33bf141fbc20603b3db53dfcc3bda65f3a
LuckyE <1024053774@qq.com>
2024-01-21 00:06:00 +0800

    Update Chapter04_HomeWork.java

--- src/chapter04/Chapter04_HomeWork.java
+++ src/chapter04/Chapter04_HomeWork.java
@@ -3,16 +3,17 @@ package chapter04;
 
 
 
-//某人有100，000元，每经过一次路口，需要交费，
-// 规则如下：
-// 1）当现金>50000时，每次交5%
-// 2）当现金<=50000时，每次交1000
-// 编程计算该人可以经过多少次路口，
-// 要求：使用while break方式完成
+
 
 import java.util.Scanner;
 
 public class Chapter04_HomeWork {
+    //某人有100，000元，每经过一次路口，需要交费，
+    // 规则如下：
+    // 1）当现金>50000时，每次交5%
+    // 2）当现金<=50000时，每次交1000
+    // 编程计算该人可以经过多少次路口，
+    // 要求：使用while break方式完成
     public static void main(String[] args) {
         double now_money = 100000;
         int count = 0;
