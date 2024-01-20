40cc661e18cf0243be980c14ee8049ea45b8aaa6
LuckyE <1024053774@qq.com>
2024-01-20 22:39:48 +0800

    Chapter04_HomeWork7

--- src/chapter04/Chapter04_HomeWork.java
+++ src/chapter04/Chapter04_HomeWork.java
@@ -93,7 +93,7 @@ class Chapter04_HomeWork4{
 class Chapter04_HomeWork6{
     //输出1-100之间的不能被5整除的数，每5个一行
     public static void main(String[] args) {
-        int count = 0;
+    int count = 0;
     for( int i = 1;i <= 100;i++){
             if (i % 5 != 0){
                 System.out.print(i);
@@ -105,5 +105,18 @@ class Chapter04_HomeWork6{
                 }
             }
     }
+    }
+}
+
+class Chapter04_HomeWork7{
+    //输出小写的a-z以及大写的Z-A
+    public static void main(String[] args) {
+        for (char small = 'a';small <='z';small++){
+            System.out.print(small + " ");
+        }
+        System.out.print('\n');
+        for (char big = 'Z';big >= 'A';big--){
+            System.out.print(big + " ");
+        }
     }
 }
