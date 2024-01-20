ae6ed441920d408a21fbc6ff658ab24634fb39ce
LuckyE <1024053774@qq.com>
2024-01-20 22:12:53 +0800

    Chapter04_HomeWork3

--- src/chapter04/Chapter04_HomeWork.java
+++ src/chapter04/Chapter04_HomeWork.java
@@ -46,3 +46,18 @@ class Chapter04_HomeWork2{
 
     }
 }
+
+
+class Chapter04_HomeWork3{
+    //判断一个年份是否为闰年
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("请输入年份(整数)");
+        int year = myScanner.nextInt();
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+            System.out.println(year + "年是闰年");
+        } else {
+            System.out.println(year + "年不是闰年");
+        }
+    }
+}
