c6b98d97ad41fd85bb0cf92a9baae0d76bf30e84
LuckyE <1024053774@qq.com>
2024-01-23 22:09:00 +0800

    列不确定的二维数组

--- src/chapter05/TwoDimensionalArray.java
+++ src/chapter05/TwoDimensionalArray.java
@@ -16,3 +16,42 @@ public class TwoDimensionalArray {
         }
     }
 }
+
+class TwoDimensionalArray02 {
+    public static void main(String[] args) {
+//        int array[][] = new int[2][3];
+        int array[][];
+        array = new int[2][3];
+        for (int i = 0;i < array.length;i++){
+            for (int j = 0;j < array[i].length;j++){
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.print('\n');
+        }
+    }
+}
+
+class TwoDimensionalArray03 {
+    public static void main(String[] args) {
+        //
+        //i=0 : 1
+        //i=1 : 2 2
+        //i=2 : 3 3 3
+        //
+        int arr[][] = new int[10][];//创建二维数组的一维数组的个数
+        for ( int i = 0; i < arr.length;i++){
+            arr[i] = new int[i + 1];//开辟一维数组的空间
+            //遍历一维数组,并给一维数组的每个元素赋值
+            for ( int j = 0; j < arr[i].length;j++){
+                arr[i][j] = i + 1;
+            }
+        }
+
+        for (int i = 0;i < arr.length;i++){
+            for (int j = 0; j < arr[i].length;j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
