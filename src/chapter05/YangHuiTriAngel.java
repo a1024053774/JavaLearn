464dbd40defafc5b79b226b531514b06d2b15c97
LuckyE <1024053774@qq.com>
2024-01-23 22:59:32 +0800

    Update YangHuiTriAngel.java

--- src/chapter05/YangHuiTriAngel.java
+++ src/chapter05/YangHuiTriAngel.java
@@ -7,25 +7,25 @@ public class YangHuiTriAngel {
     // arr[i][j] = arr[i-1][j] + arr[i-1][j-1]
     public static void main(String[] args) {
         int array[][] = new int[20][];
-        for (int i = 0;i < array.length;i++){
+        for (int i = 0; i < array.length; i++) {
             array[i] = new int[i + 1];
             //给每个一维数组赋值
-            for ( int j = 0;j < array[i].length;j++){
-                if ( j == 0 ||j == array[i].length - 1){
+            for (int j = 0; j < array[i].length; j++) {
+                if (j == 0 || j == array[i].length - 1) {
                     array[i][j] = 1;
                 } else {
-                    array[i][j] = array[i-1][j] + array[i-1][j-1];
+                    array[i][j] = array[i - 1][j] + array[i - 1][j - 1];
                 }
 //                System.out.print(array[i][j] + " ");
             }
 //            System.out.println();
 
         }
-    for(int i = 0;i < array.length;i++){
-        for (int j = 0; j < array[i].length;j++){
-            System.out.print(array[i][j] + " ");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
     }
 }
