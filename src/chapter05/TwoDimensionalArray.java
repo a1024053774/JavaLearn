468053251469b34714ee80755a56bde37e735b3a
LuckyE <1024053774@qq.com>
2024-01-23 22:20:57 +0800

    Update TwoDimensionalArray.java

--- src/chapter05/TwoDimensionalArray.java
+++ src/chapter05/TwoDimensionalArray.java
@@ -55,3 +55,16 @@ class TwoDimensionalArray03 {
         }
     }
 }
+
+class TwoDimensionalArray04 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int arr[][] = {{4,6},{1,4,5,7},{-2}};
+        for (int i = 0;i < arr.length;i++){
+            for (int j = 0;j < arr[i].length;j++){
+                sum += arr[i][j];
+            }
+        }
+        System.out.println(sum);
+    }
+}
