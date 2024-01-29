36da059f72d2c0d29a7485e30c7f6551ad38af93
LuckyE <1024053774@qq.com>
2024-01-29 14:30:26 +0800

    Update HomeWork02.java

--- src/Chapter06/HomeWork02.java
+++ src/Chapter06/HomeWork02.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class HomeWork02 {
     public static void main(String[] args) {
-        //2.编写类A02，定义方法find，实现查找某字符串数组中的元素查找，并返回索引l，如果找不到，返回-1
+        //2.编写类A02，定义方法find，实现查找某字符串数组中的元素查找，并返回索引，如果找不到，返回-1
 
         A02 a02 = new A02();
         char array[] = {'a','c','d','%','p','$'};
