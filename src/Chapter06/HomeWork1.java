fe5597ce933ccba27accbd3e3b46b065d54f52dc
LuckyE <1024053774@qq.com>
2024-01-29 14:22:22 +0800

    Update HomeWork1.java

--- src/Chapter06/HomeWork1.java
+++ src/Chapter06/HomeWork1.java
@@ -5,19 +5,29 @@ public class HomeWork1 {
     public static void main(String[] args) {
         A01 a01 = new A01();
         double array[] = {1.1,8.8,5.5,7.7,9.9};
-        double max = a01.max(array);
-        System.out.println("数组array的最大值是" + max);
+        Double max = a01.max(array);
+        if (max != null){
+            System.out.println("数组array的最大值是" + max);
+        } else {
+            System.out.println("数组array输入错误,不能为null或者{}");
+        }
+
     }
 }
 
 class A01{
-    public double max(double array[]){
-        double maxm =array[0];
-        for (int i = 1;i < array.length;i++){
-            if (array[i] > maxm){
-                maxm = array[i];
+    public Double max(double array[]){//Double为对象,可以赋空值
+        //保证数组不为空
+        if (array != null && array.length > 0){//先判断array是否为空再判断length>0,避免了输入数组为null时的错误
+            double maxm =array[0];
+            for (int i = 1;i < array.length;i++){
+                if (array[i] > maxm){
+                    maxm = array[i];
+                }
             }
+            return maxm;
+        } else {
+            return null;
         }
-        return maxm;
     }
 }
