26993895520f4e4aa895b1ab1cbce17529c00689
LuckyE <1024053774@qq.com>
2024-01-30 17:03:14 +0800

    Create packageLearn.java

---
+++ src/Chapter07/com/packageLearn.java
@@ -0,0 +1,16 @@
+package Chapter07.com;
+
+import java.util.Arrays;
+
+public class packageLearn {
+    public static void main(String[] args) {
+        int arr[] = {9,-8,8,0,13,-7};
+        //使用系统提供的Arrays的sort方法进行排序
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+
+        }
+    }
+}
