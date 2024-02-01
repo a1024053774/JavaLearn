0d4551ac743cbec0031aa81448fbd0014173297d
LuckyE <1024053774@qq.com>
2024-02-01 23:02:18 +0800

    动态绑定

---
+++ src/Chapter07/com/poly_/dynamic_/DynamicBinding.java
@@ -0,0 +1,36 @@
+package Chapter07.com.poly_.dynamic_;
+
+public class DynamicBinding {
+    public static void main(String[] args) {
+        A a = new B();
+        System.out.println(a.sum());
+        System.out.println(a.sum1());
+    }
+}
+
+class A{//父类
+    public int i = 10;
+    //动态绑定
+    public int sum(){
+        return getI() + 10;
+    }
+    public int sum1(){
+        return i + 10;
+    }
+    public int getI(){
+        return i;
+    }
+}
+
+class B extends A{
+    public int i = 20;
+//    public int sum(){
+//        return getI() + 20;
+//    }
+//    public int sum1(){
+//        return i + 10;
+//    }
+    public int getI(){
+        return i;
+    }
+}
