be3a5d0a00bd20646756ce323f3f0bfbd1295bd2
LuckyE <1024053774@qq.com>
2024-02-03 22:05:45 +0800

    Create Homework02.java

---
+++ src/Chapter07/com/homework/hm02/Homework02.java
@@ -0,0 +1,8 @@
+package Chapter07.com.homework.hm02;
+
+public class Homework02 {
+    //public  公共的  都可以访问
+    //protected 受保护的  同一个包下可以访问,以及不同包里的子类可以访问
+    //default   默认的  同一个包下都可以访问
+    //private       私有的  只有本类可以访问
+}
