aab4fe9b74b561a379c15556d1c8909614357647
LuckyE <1024053774@qq.com>
2024-02-04 11:46:04 +0800

    添加题目要求

--- src/Chapter07/com/homework/hm05/HomeWork05.java
+++ src/Chapter07/com/homework/hm05/HomeWork05.java
@@ -1,5 +1,12 @@
 package Chapter07.com.homework.hm05;
 
+/**
+ * 设计父类一员工类。子类：工人类，农民类，教师类，科学家类，服务生类
+ * （1）其中工人，农民，服务生只有基本工资
+ * （2）教师除基本工资外，还有课酬（元/天）
+ * （3）科学家除基本工资外，还有年终奖
+ * （4）编写一个测试类，将各种类型的员工的全年工资打印出来
+ */
 public class HomeWork05 {
     public static void main(String[] args) {
         Employee employee = new Worker("工人", 5000);
